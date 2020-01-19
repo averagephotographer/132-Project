50a4ae38d37a3ce8f1dcf63f31bdbae7b334b7d6
Chris <ctu007@latech.edu>
2020-01-19 17:37:16 -0600

    Started on the GUI code, please give feedback on the formatting of questinos and answers

--- GUI Code/new.py
+++ GUI Code/new.py
@@ -1,2 +1,54 @@
-print "Hello World"
-print "Hello World!"
+# This is the skeleton for the game GUI
+
+from Tkinter import *
+
+DEBUG = False
+
+# question class
+class Q(object):
+    # inputs the question number and the answer
+    def __init__(self, number, text):
+        self.number = number
+        self.text = text
+        self.question = {}
+        self.answers = {}
+        if DEBUG == True:
+            print "number: {}, text: {}".format(number, text)
+    
+    @property
+    def number(self):
+        return self._number
+    
+    @number.setter
+    def number(self, value):
+        self._number = value
+
+    @property
+    def text(self):
+        return self._text
+    
+    @text.setter
+    def text(self, string):
+        self._text = string
+    
+    @property
+    def question(self):
+        return self._question
+    
+    @question.setter
+    def question(self, string):
+        self._question = string
+
+    def addAnswers(self, number, dict):
+        pass
+
+    # this should eventually return:
+    # The question then four answers in random order
+    def __str__(self):
+        return "Question {} is:\n{}".format(self.number, self.text)
+
+
+
+q1 = Q(0, "How are you?")
+
+print q1
