3aaefc54eeef3625fec3da0538c84ca68293218e
AndresTorres1963 <58526830+AndresTorres1963@users.noreply.github.com>
2020-02-02 13:17:53 -0600

    Update Final Pi project code.py

--- GPIO/Final Pi project code.py
+++ GPIO/Final Pi project code.py
@@ -1,35 +1,38 @@
-import RPi.GPIO as GPIO
+##import RPi.GPIO as GPIO
 from time import sleep
 
 #Setting up the LED and switch pin numbers
 wrong = 22
 correct = 20
-button_1 = 12
-#buttons = [14, 26, 24]
-button_2 = 14
-button_3 = 26
-button_4 = 24
+##button_1 = 12
+buttons = [12, 16, 24, 26]
+##button_2 = 16
+##button_3 = 26
+##button_4 = 24
 
 #Use the Broadcam pin code
 GPIO.setmode(GPIO.BCM)
 
 #Setup the LED and switch pins
-GPIO.setmode(GPIO.BCM)
+##GPIO.setmode(GPIO.BCM)
 GPIO.setup(wrong, GPIO.OUT)
-GPIO.setup(correct, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(button_1, GPIO.OUT)
-GPIO.setup(button_2, GPIO.OUT)
-GPIO.setup(button_3, GPIO.OUT)
-GPIO.setup(button_4, GPIO.OUT)
+GPIO.setup(correct, GPIO.OUT)
+GPIO.setup(buttons, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+##GPIO.setup(button_2, GPIO.OUT)
+##GPIO.setup(button_3, GPIO.OUT)
+##GPIO.setup(button_4, GPIO.OUT)
 #####################
-ans = ["a", "b", "c", "D"]
-button_1 = ans[0]
+##ans = ["a", "b", "c", "D"]
+##button_1 = ans[0]
 #####################
 
 #GPIO pins
 while(True):
     #light LED if answer is true
-    if (answer == self.ans[0]):
+    if (GPIO.input(buttons[0]) == GPIO.HIGH):
         GPIO.output(correct, GPIO.HIGH)
-    else:
-        GPIO.output(wrong, GPIO.HIGH)
+    for i in range(1,len(buttons)):
+        if(GPIO.input(buttons[i]) == GPIO.HIGH):
+            GPIO.output(wrong, GPIO.HIGH)
+##    else:
+##        GPIO.output(wrong, GPIO.HIGH)
