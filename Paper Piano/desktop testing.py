9a1aa8ae278540f6c24276fd6c92b00f63d44dd0
averagephotographer <cdtv1473@gmail.com>
2020-02-06 16:42:51 -0600

    paper piano

---
+++ Paper Piano/desktop testing.py
@@ -0,0 +1,48 @@
+##################################
+# Name: Christopher Tullier
+# Date: 1/29/2020
+# Description: Paper Piano
+##################################
+# import RPi.GPIO as GPIO
+from time import sleep, time
+# import pygame
+from array import array
+from waveform_vis import WaveformVis
+from math import pi, sin
+DEBUG = True
+
+MIXER_FREQ = 44100
+MIXER_SIZE = -16
+MIXER_CHANS  = 1
+MIXER_BUFF = 1024
+frequency = 261.6
+# builds an array of samples for the curent note
+def build_samples():
+    # calculate the period and amplitude of the note's wave
+    period = int(round(MIXER_FREQ / frequency))
+    amplitude = 2 ** (abs(MIXER_SIZE) - 1) - 1
+    # initialize the note's sampels (using an array of signed 16-bit 'shorts")
+    samples = array("h", [0] * period)
+
+    if DEBUG == True: 
+        print "Period: {}, Amp: {}, Smpls: {}".format(period, amplitude, samples) 
+
+    #generate the note's samples
+    print "period: {}\namplitude: {}\nsamples: {}".format(period, amplitude, samples)
+    
+    for t in range(period):
+        # if (t < period / 2):
+        #     samples[t] = (amplitude * t) / period
+        # else:
+        #     samples[t] = ((amplitude * t) / period) - 2
+        
+        
+        samples[t] = int(amplitude * (sin(((2 * pi) / period) * t)))
+
+
+
+    vis = WaveformVis()
+    vis.visSamples(samples, "waveform_name")
+    return samples
+
+build_samples()
