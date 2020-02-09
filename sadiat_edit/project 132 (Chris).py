e7e6936a937c81ed6c59fa6d16541e4dfcf26842
averagephotographer <cdtv1473@gmail.com>
2020-02-08 18:54:49 -0600

    new Chris

--- sadiat_edit/project 132 (Chris).py
+++ sadiat_edit/project 132 (Chris).py
@@ -4,11 +4,9 @@ from time import sleep
 
 class Q(object):
     def __init__(self, name):
-        self.text = {} ##stores the dif and correct answer in a dictionary
-        self.answers = [] ### contains the options including the correct answer\
-        self.questions = [] ### contains the questions
-
-
+        self.text = {}          # stores the dif and correct answer in a dictionary
+        self.answers = []       # contains the options including the correct answer
+        self.questions = []     # contains the questions
 
     @property
     def text(self):
@@ -34,34 +32,31 @@ class Q(object):
     def questions(self, string):
         self._questions = string
 
-    
-    def addcorrect(self, dif, correct): ##adds the question as the key and the correct answer as the data value in the dictionary
+    def addCorrect(self, dif, correct):     # adds the question as the key and the correct answer as the data value in the dictionary
         self._text[dif] = correct
-        #self._questions = question
-        self._answers.append(correct) ## adds the correct answer into the self.answers list
+        self._answers.append(correct)       # adds the correct answer into the self.answers list
 
-    def addquestions(self, q):
+    def addQuestions(self, q):
         self._questions.append(q)
 
-    def addoptions(self, a,b,c): ##adds the wrong answers to a list ##adds the wrong answers into the self.answers list
+    def addOptions(self, a,b,c):            # adds the wrong answers to a list ##adds the wrong answers into the self.answers list
         self._answers.append(a)
         self._answers.append(b)
         self._answers.append(c)
 
     def shuffle(self):
-        random.shuffle(self.answers) ## function that shuffles the answers
-
+        random.shuffle(self.answers)        # function that shuffles the answers
 
     def __str__(self):
         pass
 
-    #this is the game function
+# this is the game function
 class Riddles(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
-        self.allQuestions = [] ## adds all the question variables into a list
+        self.allQuestions = [] ## will add all the question variables into a list
 
-    ## creates the 20 questions
+    # creates the 20 questions
     def questions(self):
         global currentQuestion
         self.count = 0
@@ -70,27 +65,27 @@ class Riddles(Frame):
         q2 = Q("question2")
         q3 = Q("question3")
 
-        q1.addquestions("How are you?") ## calls the add questions function
-        q2.addquestions("who's on first?")
-        q3.addquestions("difficult job?")
+        q1.addQuestions("How are you?")             # calls the add questions function
+        q2.addQuestions("who's on first?")
+        q3.addQuestions("difficult job?")
 
-        q1.addcorrect(1, "good")
-        q2.addcorrect(2, "me")
-        q3.addcorrect(3, "computer science")
+        q1.addCorrect(1, "good")
+        q2.addCorrect(2, "me")
+        q3.addCorrect(3, "computer science")
 
-        q1.addoptions("bleh", "okay", "not good" )## calls the add options function
+        q1.addOptions("bleh", "okay", "not good" )  # calls the add options function
         
-        q1.shuffle() ## calls the shuffle function
+        q1.shuffle() # calls the shuffle function
         
-        q2.addoptions("you", "andres", "chris")
+        q2.addOptions("you", "andres", "chris")
         
         q2.shuffle()
         
-        q3.addoptions("biology", "physics", "child education" )
+        q3.addOptions("biology", "physics", "child education" )
         
         q3.shuffle()
 
-        self.questionlist(q1)## append each question into a list
+        self.questionlist(q1)       # append each question into a list
         self.questionlist(q2)
         self.questionlist(q3)
 
@@ -101,8 +96,10 @@ class Riddles(Frame):
         self.allQuestions.append(q)
 
 
-    #def iterator(self, event):
-        #if(button == self.currentQuestion.text[self.count]):
+    def iterator(self, event):
+        if 
+        if(Button == self.currentQuestion.text[self.count]):
+            self.count += 1
                       
             
 
@@ -135,7 +132,7 @@ window = Tk()
 window.title("How Smart are You?")
 k = Riddles(window)
 k.play()
-#window.bind("<Button-1>", k.iterator)
+# window.bind("<Button-1>", k.iterator)
 
 window.mainloop()
 
